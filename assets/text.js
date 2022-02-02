c87c7a635865240845764e8b4dfbb61bb3682494
Mason Grose <92874161+MasonGrose@users.noreply.github.com>
2022-02-02 09:27:35 -0800

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -30,3 +30,24 @@ var data = [
   // And done!
   return out[Math.floor(Math.random() * out.length)];
 }
+function newRoll(){
+    var tile = document.getElementById("tile");
+    document.getElementById("tile").innerHTML = "";
+    if(tile.innerHTML == ""){
+        let out = [];
+  
+  // Loop through the master entries.
+  for (let i = 0; i < data.length; ++i) {
+      // Push the value over and over again according to its
+      // weight.
+      for (let j = 0; j < data[i][1]; ++j) {
+          out.push(data[i][0]);
+      }
+  }
+  
+  // And done!
+  return out[Math.floor(Math.random() * out.length)];
+    }else{
+        console.log("error");
+    }
+}
