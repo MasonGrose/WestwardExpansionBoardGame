51e872938bea0b13d31a511c22671e8f31f37b1e
Mason Grose <92874161+MasonGrose@users.noreply.github.com>
2022-02-02 08:48:23 -0800

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -1,17 +1,17 @@
 var data = [
     ["Get the plague, loose 15 tiles", 1],
     ["Get the plague, loose 10 tiles", 2],
-    ["Move 2 tiles.", 3],
-    ["Move 3 tiles.", 2],
+    ["Move 2 tiles.", 6],
+    ["Move 3 tiles.", 4],
       ["Get stuck in the snow, loose 2 tiles.", 1],
-      ["Move 1 tiles.", 5],
-      ["Move 9 tiles.", 1],
+      ["Move 1 tiles.", 7],
+      ["Move 9 tiles.", 3],
       ["You ate raw meet, loose 2 tiles.", 3],
-      ["You go over hilly ground, loose 1 tile.", 5],
+      ["You go over hilly ground, loose 1 tile.", 2],
       ["You got the typhoid fever, loose 2 tiles", 2],
       ["You got dysentery, loose 3 tiles", 1],
       ["You got Cholera, loose 3 tiles", 1],
-      ["You meet a nice group of Indians, they help you along 5 tiles.", 6],
+      ["You meet a nice group of Indians, they help you along 5 tiles.", 4],
       ["You met a mean group of Indians, they fought you off. Loose 5 tiles.", 5],
   ];
   document.getElementById("tile").innerHTML = getTile();
