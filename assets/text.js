e1bba2fe2096d8f1ce58703a32bc3d819864e1bb
Mason Grose <92874161+MasonGrose@users.noreply.github.com>
2022-02-02 09:47:02 -0800

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -13,6 +13,11 @@ var data = [
       ["You got Cholera, loose 3 tiles", 1],
       ["You meet a nice group of Indians, they help you along 5 tiles.", 4],
       ["You met a mean group of Indians, they fought you off. Loose 5 tiles.", 5],
+    ["Uh Oh! Your wagon wheel broke, loose 2 tiles.", 2],
+    ["Run into buffalo, strike it rich with meet. Move 2 tiles.", 2],
+    ["Run into buffalo, they ran over your wagon. Loose 2 tiles.", 2],
+    ["You've ran out of wood. Loose 1 tile.", 1],
+    ["You've gotten mauled by a bear. Loose 5 tiles.", 0.1],
   ];
   document.getElementById("tile").innerHTML = getTile();
   function getTile(){
