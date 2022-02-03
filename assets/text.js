237be3f389051295d0f641c20e3fd5c9511367d7
Mason Grose <92874161+MasonGrose@users.noreply.github.com>
2022-02-03 10:53:19 -0800

    yes

--- assets/text.js
+++ assets/text.js
@@ -46,3 +46,13 @@ function moveTile(amount){
   document.getElementById("user").className += "mr-4";
   console.log("You moved "+amount+" tiles.");
 }
+function spliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  while (arr.length > 0) {
+      const chunk = arr.splice(0, chunkSize);
+      res.push(chunk);
+  }
+  return res;
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(spliceIntoChunks(arr, 2));
