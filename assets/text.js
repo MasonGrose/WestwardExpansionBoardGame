f66859be2c3135b4136b43941c53f56ac9144c10
Mason Grose <92874161+MasonGrose@users.noreply.github.com>
2022-02-08 09:40:34 -0800

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -22,10 +22,10 @@ var data = [
 begText1 = document.getElementById("tile").innerHTML;
 buttonText = document.getElementById("buttonRoll").innerHTML;
 betText = "To play the game, press roll again and your tile will move. Your goal is to get to the end without loosing more than 30 tiles in one game. Good luck!";
-if(begText1 == betText){
-    buttonText = "Start game!";
+if(document.getElementById("tile").innerHTML == "To play the game, press roll again and your tile will move. Your goal is to get to the end without loosing more than 30 tiles in one game. Good luck!"){
+    document.getElementById("buttonRoll").innerHTML = "Start game!";
 }else{
-    buttonText = "Roll again!";
+    document.getElementById("buttonRoll").innerHTML = "Roll again!";
 }
   //document.getElementById("tile").innerHTML = getTile();
   function getTile(){
