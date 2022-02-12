71cb4c4691109d2425109cd8404941dd162643c7
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2022-02-12 14:39:38 -0800

    Update text.js

--- assets/text.js
+++ assets/text.js
@@ -1,22 +1,21 @@
-@@ -1,344 +0,0 @@
 var data = [
-    ["Get the plague, loose 15 tiles", 1, -15],
-    ["Get the plague, loose 10 tiles", 1, -10],
+    ["Get the plague, loose 15 tiles", 0.2, -15],
+    ["Get the plague, loose 10 tiles", 0.2, -10],
     ["Move 2 tiles.", 6, 2],
     ["Move 3 tiles.", 4, 3],
     ["Get stuck in the snow, loose 2 tiles.", 1, -2],
     ["Move 1 tiles.", 7, 1],
-    ["Move 9 tiles.", 3, 9],
-    ["You ate raw meet, loose 2 tiles.", 3, -2],
-    ["You go over hilly ground, loose 1 tile.", 2, -1],
-    ["You got the typhoid fever, loose 2 tiles", 2, -3],
-    ["You got dysentery, loose 3 tiles", 1, -3],
+    ["Move 9 tiles.", 0.9, 9],
+    ["You ate raw meet, loose 2 tiles.", 1, -2],
+    ["You go over hilly ground, loose 1 tile.", 0.8, -1],
+    ["You got the typhoid fever, loose 2 tiles", 0.4, -3],
+    ["You got dysentery, loose 3 tiles", 0.2, -3],
     ["You got Cholera, loose 3 tiles", 1, -3],
-    ["You meet a nice group of Indians, they help you along 5 tiles.", 4, 5],
-    ["You met a mean group of Indians, they fought you off. Loose 5 tiles.", 5, -5],
-    ["Uh Oh! Your wagon wheel broke, loose 2 tiles.", 2, -2],
+    ["You meet a nice group of Indians, they help you along 5 tiles.", 0.4, 5],
+    ["You met a mean group of Indians, they fought you off. Loose 5 tiles.", 0.7, -5],
+    ["Uh Oh! Your wagon wheel broke, loose 2 tiles.", 0.6, -2],
     ["Run into buffalo, strike it rich with meet. Move 2 tiles.", 2, 2],
-    ["Run into buffalo, they ran over your wagon. Loose 2 tiles.", 2, -2],
+    ["Run into buffalo, they ran over your wagon. Loose 2 tiles.", 0.7, -2],
     ["You've ran out of wood. Loose 1 tile.", 1, -1],
     ["You've gotten mauled by a bear. Loose 5 tiles.", 0.1, -5],
   ];
@@ -25,11 +24,13 @@ buttonText = document.getElementById("buttonRoll").innerHTML;
 betText = "To play the game, press roll again and your tile will move. Your goal is to get to the end without loosing more than 30 tiles in one game. Good luck!";
 if(document.getElementById("tile").innerHTML == "To play the game, press roll again and your tile will move. Your goal is to get to the end without loosing more than 30 tiles in one game. Good luck!"){
     document.getElementById("buttonRoll").innerHTML = "Start game!";
+    let playerSpot = sessionStorage.setItem("playerSpot", 1);
 }else{
     document.getElementById("buttonRoll").innerHTML = "Roll again!";
 }
+  let playerSpot = sessionStorage.getItem("playerSpot");
   //document.getElementById("tile").innerHTML = getTile();
-  function getTile(){
+  function getTile() {
   let out = [];
   let num = [];
   
@@ -44,9 +45,16 @@ if(document.getElementById("tile").innerHTML == "To play the game, press roll ag
   }
   
   // And done!
-  var output = out[Math.floor(Math.random() * out.length)];
-  var text = spliceIntoChunks(output, 1);
-  moveTile(text[1])
+  let output = out[Math.floor(Math.random() * out.length)];
+  let text = spliceIntoChunks(output, 1);
+  let text1 = parseInt(text[1], 10);
+  let playerSpot = sessionStorage.getItem("playerSpot");
+  let calcEnd = playerSpot + text1;
+  console.log("CalcEnd: " + calcEnd);
+  if (calcEnd < 1) {
+    getTile()
+  }
+  moveTile(text[1], playerSpot)
   return text[0];
   //return num[Math.floor(Math.random() * num.length)];
 }
@@ -55,9 +63,20 @@ function newRoll(){
     document.getElementById("tile").innerHTML = getTile();
     document.getElementById("buttonRoll").innerHTML = "Roll again!";
 }
-function moveTile(amount){
-  document.getElementById("user").className += "mr-".amount;
-  console.log("You moved "+amount+" tiles.");
+function moveTile(amount, playerSpot1){
+  let amount1 = parseInt(amount, 10);
+  let play1 = parseInt(playerSpot1, 10);
+  var updatedSpot = play1 + amount1;
+  if (updatedSpot < 1) {
+    if(play1 < 1){
+      sessionStorage.setItem("playerSpot", 1);
+      getTile();
+    }else{
+    getTile();
+    }
+  }
+  let playerSpot = sessionStorage.setItem("playerSpot", updatedSpot);
+  console.log("You moved "+amount+" tiles. Updated spot: "+updatedSpot);
 }
 function spliceIntoChunks(arr, chunkSize) {
   const res = [];
