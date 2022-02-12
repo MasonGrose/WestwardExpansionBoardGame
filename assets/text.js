6c7f46c777908686a08632b0d33e8fa44933a304
The MG | GallearTech <63264652+The-MG@users.noreply.github.com>
2022-02-12 15:58:05 -0800

    done

--- assets/text.js
+++ assets/text.js
@@ -1,6 +1,6 @@
 var data = [
-    ["Get the plague, loose 15 tiles", 0.2, -15],
-    ["Get the plague, loose 10 tiles", 0.2, -10],
+    ["Get the plague, loose 5 tiles", 0.2, -5],
+    ["Get the plague, loose 2 tiles", 0.2, -2],
     ["Move 2 tiles.", 6, 2],
     ["Move 3 tiles.", 4, 3],
     ["Get stuck in the snow, loose 2 tiles.", 1, -2],
@@ -11,13 +11,13 @@ var data = [
     ["You got the typhoid fever, loose 2 tiles", 0.4, -3],
     ["You got dysentery, loose 3 tiles", 0.2, -3],
     ["You got Cholera, loose 3 tiles", 1, -3],
-    ["You meet a nice group of Indians, they help you along 5 tiles.", 0.4, 5],
-    ["You met a mean group of Indians, they fought you off. Loose 5 tiles.", 0.7, -5],
-    ["Uh Oh! Your wagon wheel broke, loose 2 tiles.", 0.6, -2],
+    ["You meet a nice group of Indians, they help you along 2 tiles.", 0.4, 2],
+    ["You met a mean group of Indians, they fought you off. Loose 2 tiles.", 0.7, -2],
+    ["Uh Oh! Your wagon wheel broke, loose 1 tiles.", 0.6, -1],
     ["Run into buffalo, strike it rich with meet. Move 2 tiles.", 2, 2],
-    ["Run into buffalo, they ran over your wagon. Loose 2 tiles.", 0.7, -2],
+    ["Run into buffalo, they ran over your wagon. Loose 1 tiles.", 0.7, -1],
     ["You've ran out of wood. Loose 1 tile.", 1, -1],
-    ["You've gotten mauled by a bear. Loose 5 tiles.", 0.1, -5],
+    ["You've gotten mauled by a bear. Loose 3 tiles.", 0.1, -3],
   ];
 begText1 = document.getElementById("tile").innerHTML;
 buttonText = document.getElementById("buttonRoll").innerHTML;
@@ -25,14 +25,19 @@ betText = "To play the game, press roll again and your tile will move. Your goal
 if(document.getElementById("tile").innerHTML == "To play the game, press roll again and your tile will move. Your goal is to get to the end without loosing more than 30 tiles in one game. Good luck!"){
     document.getElementById("buttonRoll").innerHTML = "Start game!";
     let playerSpot = sessionStorage.setItem("playerSpot", 1);
-}else{
+    document.getElementById("s1-owner").classList.add("triangle");
+  }else{
     document.getElementById("buttonRoll").innerHTML = "Roll again!";
 }
   let playerSpot = sessionStorage.getItem("playerSpot");
   //document.getElementById("tile").innerHTML = getTile();
   function getTile() {
   let out = [];
-  let num = [];
+  let playerSpot = sessionStorage.getItem("playerSpot");
+  document.getElementById("s"+playerSpot+"-owner").classList.remove("triangle");
+  if(playerSpot >= 30){
+    wonGame()
+  }
   
   // Loop through the master entries.
   for (let i = 0; i < data.length; ++i) {
@@ -48,11 +53,16 @@ if(document.getElementById("tile").innerHTML == "To play the game, press roll ag
   let output = out[Math.floor(Math.random() * out.length)];
   let text = spliceIntoChunks(output, 1);
   let text1 = parseInt(text[1], 10);
-  let playerSpot = sessionStorage.getItem("playerSpot");
+  
   let calcEnd = playerSpot + text1;
   console.log("CalcEnd: " + calcEnd);
   if (calcEnd < 1) {
-    getTile()
+    if(playerSpot < 1){
+      sessionStorage.setItem("playerSpot", 1);
+      getTile();
+    }else{
+    getTile();
+    }
   }
   moveTile(text[1], playerSpot)
   return text[0];
@@ -68,15 +78,18 @@ function moveTile(amount, playerSpot1){
   let play1 = parseInt(playerSpot1, 10);
   var updatedSpot = play1 + amount1;
   if (updatedSpot < 1) {
-    if(play1 < 1){
-      sessionStorage.setItem("playerSpot", 1);
-      getTile();
-    }else{
     getTile();
-    }
+  }
+  if(updatedSpot > 30){
+    wonGame()
   }
   let playerSpot = sessionStorage.setItem("playerSpot", updatedSpot);
-  console.log("You moved "+amount+" tiles. Updated spot: "+updatedSpot);
+  document.getElementById("s"+updatedSpot+"-owner").classList.add("triangle");
+  return console.log("You moved "+amount+" tiles. Updated spot: "+updatedSpot);
+  
+}
+function wonGame (){
+  window.location.replace("./won.html");
 }
 function spliceIntoChunks(arr, chunkSize) {
   const res = [];
